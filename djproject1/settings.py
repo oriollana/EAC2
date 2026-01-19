3369a7524a52dd2edaf2f67296b55bfea0c17e9f
isard <isard@debian>
2026-01-19 18:16:42 +0100

    segundo commit

--- djproject1/settings.py
+++ djproject1/settings.py
@@ -25,12 +25,13 @@ SECRET_KEY = 'django-insecure-6o@o^d%hy+n1svnic&&mdrj_7zq_8m1(_m-v70871(=at!6zt*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*',]
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
